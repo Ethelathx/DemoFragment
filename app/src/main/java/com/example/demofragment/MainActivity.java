f07c1eea4959a3e72b722c7d374d71da7a7f7645
19042228 <19042228@myrp.edu.sg>
2021-05-26 15:39:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demofragment/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.demofragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+
+        //---------------Display Frag 1-------------
+        Fragment f1 = new FragmentFirst();
+        ft.replace(R.id.frame1, f1);
+        //---------------Display Frag 1-------------
+
+
+        //---------------Display Frag 2-------------
+        Fragment f2 = new FragmentSecond();
+        ft.replace(R.id.frame2, f2);
+        //---------------Display Frag 2-------------
+
+        ft.commit();
+    }
+}
